9b555b4c865b22f22250320d6518d3526130623a
BhumikaMalik01 <malik.bhumika701@gmail.com>
2022-03-15 16:40:49 +0530

    Add project files.

---
+++ StudentManagementDomainLayer/Models/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagementDomainLayer.Models
+{
+    public class Student
+    {
+    }
+}
---
+++ StudentManagementSystemAppWebAPI/Controllers/StudentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudentManagementSystemAppWebAPI.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
